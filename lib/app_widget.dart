93943322f9457f5f2b8ad133a4647ed18eff24ad
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-04-16 11:13:29 -0400

    refactor: apply the dark and light theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'app/presentations/pages/welcome/welcome_page.dart';
+import 'app/shared/themes/dark_theme.dart';
+import 'app/shared/themes/ligth_theme.dart';
+
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: LightTheme().theme,
+      darkTheme: DarkTheme().themeData,
+      home: const WelcomePage(),
+    );
   }
 }
