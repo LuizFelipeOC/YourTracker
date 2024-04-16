9cfe6d872058552c1af675db8e4190be88009a62
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-04-16 11:14:38 -0400

    feat: add basic colors

---
+++ lib/app/shared/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final class AppColors {
+  static Color darkBackground = const Color(0xFF152133);
+  static Color lightBackground = const Color(0xFFFFFFFF);
+
+  static Color grey = const Color(0xFFf0f0f0);
+
+  static Color blueDark = const Color(0xff161930);
+
+  static Color green = const Color(0xff55d4aa);
+  static Color green200 = const Color(0xff65dbb4);
+}
